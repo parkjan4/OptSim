d4275ca627f37040d45181bdc1d33ba4476aa6c1
caiobeojone <caio.beojone@epfl.ch>
2019-03-19 14:02:20 +0100

    abandonments

---
+++ Simulation/abandonments.m
@@ -0,0 +1,53 @@
+function updated_abandonment = abandonments(t, old_abandonment, customer_ID)
+% Updates the list of abandonments one a new group joins the queue.
+% 
+% =========================================================================
+% Description:
+%   Generates an updated abandonment list once a new group joins the queue.
+% 
+% -------------------------------------------------------------------------
+% Input data:
+%   t                   Current time in the simulation.
+%   old_abandonment     Old list of abandonments.
+%                       Contains: [times, customer_ID]
+%   customer_ID         ID of the group that have just joined the queue.
+% 
+% -------------------------------------------------------------------------
+% Output data:
+%   updated_abandonment Updated abandonment list sorted by time.
+% 
+% -------------------------------------------------------------------------
+% Intermidiate variables (alphabetical order):
+%   elapsed_time        time spent in queue (step).
+%   r                   pseudo-random number.
+%   prob_ab             probability of leaving at each step.
+%   t_abandonment       Actual time of leaving.
+% 
+% =========================================================================
+
+elapsed_time=0;     % Initialize the time spent in queue.
+while (true)
+    elapsed_time=elapsed_time+5;    % Updates the elapsed time.
+    r=rand();   % Generates a new random number at each trial, as customers check for abandonment every 5 minutes.
+    % Updates the probability of leaving (abandonm) the queue according to the elapsed time.
+    if elapsed_time<=5
+        prob_ab=0.1;
+    elseif elapsed_time<=10
+        prob_ab=0.3;
+    elseif elapsed_time<=20
+        prob_ab=0.4;
+    else
+        prob_ab=0.5;
+    end
+    % Check whether the customer will leave or not the queue.
+    if r<=prob_ab
+        t_abandonment=t+elapsed_time;   % Sets the time for abandonment.
+        break
+    end
+end
+
+% Generates the updated abandonment list sorted by the time for abandonment.
+updated_abandonment=sortrows([old_abandonment; t_abandonment customer_ID]);
+
+end
+
