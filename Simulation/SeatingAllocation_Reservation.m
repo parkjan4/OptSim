1f977c4796daaeb5d2aaba165689463daa37cf29
Murat Genc <murat.genc@epfl.ch>
2019-05-24 18:55:08 +0200

    seating policy final

--- Simulation/SeatingAllocation_Reservation.m
+++ Simulation/SeatingAllocation_Reservation.m
@@ -1,29 +1,38 @@
 function [assignedIDs, newCustomers, newTables] = ...
-    SeatingAllocation(oldCustomers, oldTables, customerIDs, abandonment_list)
+    SeatingAllocation_Reservation(oldCustomers, oldTables, customerIDs, abandonment_list)
 
 % ============================================================================
 % DESCRIPTION
 %
 % usage: [assignedIDs, customers, tables] = SeatingAllocation(customers, tables, NextEvent.ID, abandonment_list)
 % Allocates a seat to a refered groups, if possible. Queued users are
-% assigned according to their arrival time.
+% assigned according to the reservation policy and their arrival time.
+%
+% 4 Possible scenarios and resulting actions for groups with a reserved table:
+%
+%   1.1)  There is no waiting line and there is an empty table. Group is assigned to the table
+%   1.2)  There is no waiting line, but there are no empty tables. Group added to the abandonments list.
+%   1.3)  There is a waiting line and enough available seats in any reserved table. Group is assigned to the table
+%   1.4)  There is a waiting line and no enough available seats to fit the refered group. Group added to the abandonments list.
+%
 % 5 Possible scenarios for assignment:
-%   1)  There is no waiting line and there is an empty table.
-%   2)  There is no waiting line, but there are no empty tables. There are
+%   2.1)  There is no waiting line and there is an empty table.
+%   2.2)  There is no waiting line, but there are no empty tables. There are
 %       enough seats in a table to fit the refered group in a busy table.
-%   3)  There is no waiting line and no available seats to fit the group
+%   2.3)  There is no waiting line and no available seats to fit the group
 %       (without overpassing the table capacity).
-%   4)  There is a waiting line and enough available seats to fit the
+%   2.4)  There is a waiting line and enough available seats to fit the
 %       refered group.
-%   5)  There is a waiting line and no enough available seats to fit the
+%   2.5)  There is a waiting line and no enough available seats to fit the
 %       refered group.
-% 
-% 5 Actions (one for each scenario):
-%   1)  Assign the refered customer directly to an empty table.
-%   2)  Assign the refered group to a shared table (best as possible or random).
-%   3)  Assign the refered group to the queue.
-%   4)  Assign the refered group to a shared table (best as possible or random).
-%   5)  Assign the refered group to the queue.
+%  
+% 5 Actions for groups without a reserved table (one for each scenario):
+%
+%   2.1)  Assign the refered customer directly to an empty table.
+%   2.2)  Assign the refered group to a shared table (best as possible or random).
+%   2.3)  Assign the refered group to the queue.
+%   2.4)  Assign the refered group to a shared table (best as possible or random).
+%   2.5)  Assign the refered group to the queue.
 %
 % ----------------------------------------------------------------------------
 % PARAMETERS
@@ -46,22 +55,14 @@ function [assignedIDs, newCustomers, newTables] = ...
 
 % Take the input for the group size to reserve the tables
 
-isokinput = false;
-while ~isokinput
-   try 
-      groupsize_reserve = input('Please choose the group size to reserve tables');
-      isokinput = true;  %this line only gets executed if input does not raise an error
-   catch
-      fprintf('Invalid expression. Please retry\n\n');
-   end
-end
+reservedgroup_size = 2;
 
 assignedIDs = [];
 newCustomers=oldCustomers;
 newTables=oldTables;
+arr_grpID = [];
 
-
-% Verify whether the customer list is in the queue:
+% Verify whether the customer list is the queue:
 if ~isempty(abandonment_list)
     if isequal(customerIDs,abandonment_list(:,2))
         % Put customerIDs in arrival order:
@@ -70,31 +71,100 @@ if ~isempty(abandonment_list)
     end
 end
 
-
-
 for i=1:length(customerIDs)
     customerID=customerIDs(i);
     [~,cID] = find([oldCustomers.customerID]==customerID,1);
     group_size=oldCustomers(cID).groupsize;
-    if isempty(abandonment_list)
+    
+    % Verify if the group has a reserved table
+    if group_size == reservedgroup_size
+       % Verify if there is a group, who is eligible for a reserved table,
+       % in the queue
+        for i = 1:size(abandonment_list,1)
+            grpID = abandonment_list(i, 2);
+            [~,groupID] = find([oldCustomers.customerID]==grpID,1);
+            grpsize = oldCustomers(groupID).groupsize;
+            if grpsize == reservedgroup_size
+                arr_grpID = [arr_grpID grpID];
+            end
+        end
+        if isempty(arr_grpID)
+            % Identify empty tables with size n=reservedgroup_size
+            emptyTables = [[oldTables.availableseats] == [oldTables.tablesize]] & [oldTables.tablesize] == reservedgroup_size; 
+            if any(emptyTables)
+                    % Scenario 1.1:
+                    [~,rte] = find(emptyTables==1,1);
+                    assign_customer(newTables(rte), customerID, group_size);
+                    assignedIDs = [assignedIDs; newTables(rte).tableID, newCustomers(cID).customerID];
+            %else 
+                %Scenario 1.2
+                %Added to the abandonment list
+            end
+        else 
+            availableTables= [[oldTables.availableseats] == [oldTables.tablesize]] & [oldTables.availableseats]==group_size;
+            if any(availableTables)
+                % Scenario 1.3:
+                a_table=0;
+                for ta=1:length(availableTables)
+                    % Try to assign best table:
+                    if availableTables(ta)==1 && newTables(ta).availableseats==group_size
+                        a_table=ta;
+                        break;
+                    end
+                end
+                if newTables(a_table).availableseats == group_size
+                assign_customer(newTables(a_table), customerID, group_size);
+                assignedIDs = [assignedIDs; newTables(a_table).tableID, newCustomers(cID).customerID];
+                end
+            %else
+                 %Scenario 1.4
+                
+            end
+        end
+    else
+        if isempty(abandonment_list)
         % Identify empty tables:
-        emptyTables = [oldTables.availableseats]==[oldTables.tablesize];
-        if any(emptyTables)
-            % Scenario 1:
-            [~,te] = find(emptyTables==1,1);
-            % Prioritize group size chosen earlier
-            if group_size == groupsize_reserve && newTables(te).availableseats >= group_size
-                assign_customer(newTables(te), customerID, group_size);
-                assignedIDs = [assignedIDs; newTables(te).tableID, newCustomers(cID).customerID];
-            elseif newTables(te).availableseats >= group_size
-                assign_customer(newTables(te), customerID, group_size);
-                assignedIDs = [assignedIDs; newTables(te).tableID, newCustomers(cID).customerID];
+        emptyTables = [[oldTables.availableseats] == [oldTables.tablesize]] & [oldTables.tablesize] ~= reservedgroup_size;
+            if any(emptyTables)
+                % Scenario 2.1:
+                [~,te] = find(emptyTables==1,1);
+                if newTables(te).availableseats >= group_size
+                    assign_customer(newTables(te), customerID, group_size);
+                    assignedIDs = [assignedIDs; newTables(te).tableID, newCustomers(cID).customerID];
+                end
+            else
+                % Identify available tables (have enough seats for the group):
+                availableTables= [[oldTables.availableseats]>=group_size] & [oldTables.tablesize] ~= reservedgroup_size;
+                if any(availableTables)
+                    % Scenario 2.2:
+                    a_table=0;
+                    for ta=1:length(availableTables)
+                        % Try to assign best table:
+                        if availableTables(ta)==1 && newTables(ta).availableseats==group_size
+                            a_table=ta;
+                            break;
+                        end
+                    end
+                    % If there is no best table, assign random table:
+                    while a_table==0
+                        r=rand();
+                        newtab=floor(r*length(availableTables))+1;
+                        if availableTables(newtab)==1
+                            a_table=newtab;
+                        end
+                    end
+                    if newTables(a_table).availableseats >= group_size
+                        assign_customer(newTables(a_table), customerID, group_size);
+                        assignedIDs = [assignedIDs; newTables(a_table).tableID, newCustomers(cID).customerID];
+                    end
+        %       else
+                    % Scenario 2.3:
+                end
             end
         else
-            % Identify available tables (have enough seats for the group):
             availableTables=[oldTables.availableseats]>=group_size;
             if any(availableTables)
-                % Scenario 2:
+                % Scenario 2.4:
                 a_table=0;
                 for ta=1:length(availableTables)
                     % Try to assign best table:
@@ -115,36 +185,9 @@ for i=1:length(customerIDs)
                     assign_customer(newTables(a_table), customerID, group_size);
                     assignedIDs = [assignedIDs; newTables(a_table).tableID, newCustomers(cID).customerID];
                 end
-    %       else
-                % Scenario 3:
-            end
-        end
-    else
-        availableTables=[oldTables.availableseats]>=group_size;
-        if any(availableTables)
-            % Scenario 4:
-            a_table=0;
-            for ta=1:length(availableTables)
-                % Try to assign best table:
-                if availableTables(ta)==1 && newTables(ta).availableseats==group_size
-                    a_table=ta;
-                    break;
-                end
-            end
-            % If there is no best table, assign random table:
-            while a_table==0
-                r=rand();
-                newtab=floor(r*length(availableTables))+1;
-                if availableTables(newtab)==1
-                    a_table=newtab;
-                end
-            end
-            if newTables(a_table).availableseats >= group_size
-                assign_customer(newTables(a_table), customerID, group_size);
-                assignedIDs = [assignedIDs; newTables(a_table).tableID, newCustomers(cID).customerID];
-            end
+            end    
     %   else
-            % Scenario 5:
+            % Scenario 2.5:
         end
     end
 end
